0a7d1fa1291ce2bf3efef974a870781ab3c5b9de
clever-king-402 <122508802+clever-king-402@users.noreply.github.com>
2023-02-21 22:05:13 +0545

    Add files via upload

---
+++ Tik-Tae-Toe/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TikToe from './TikToe';
+
+function App() {
+  return (
+    <>
+    <TikToe/>
+    </>
+  );
+}
+
+export default App;
---
+++ Tik-Tae-Toe/TikToe.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Board from './Board'
+export default function TikToe() {
+  return (
+    <>
+    <h1>h3jlaksdfjkajsdfkljlasf</h1>
+         <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    </>
+  )
+}
