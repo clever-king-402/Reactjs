ec1b5c53548507bd5012bbece5e5bb482b43f9b6
clever-king-402 <122508802+clever-king-402@users.noreply.github.com>
2023-02-22 20:55:37 +0545

    Add files via upload

--- Tik-Tae-Toe/Board.js
+++ Tik-Tae-Toe/Board.js
@@ -1,25 +1,57 @@
-import React, { useState } from 'react'
+import  { useState } from 'react'
 import Square from './Square';
+
 export default function Board() {
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
     function renderSquare(i) {
         return (
           <Square
             value={first.squares[i]}
             onClick={() => {
-              const squares = first.squares.slice();;
-              squares[i] = 'X';
-              setfirst({ squares: squares });
+              const squares = first.squares.slice();
+              if (calculateWinner(squares) || squares[i]) {      return;    }
+              if(squares[i]===null){
+                squares[i] = first.xTurn ? 'X':'0';
+                setfirst({ squares: squares,xTurn:!first.xTurn });
+              }
+              else {
+                console.log('wrong entery')
+              }
             }}
           />
         );
       }
-      
+    
     const [first, setfirst] = useState({
-       squares : Array(9).fill(null) 
+       squares : Array(9).fill(null) ,
+       xTurn : true,
     })
-    const status = 'Next player: X';
+
+    const winner = calculateWinner(first.squares);
+
+    const status =winner? 'Winner ' +(winner) :'Next player: ' + (first.xTurn?'X':'0');
   return (
-    <div>
+    <div className='Tik-game-box'>
         <div className="status">{status}</div>
         <div className="board-row">
           {renderSquare(0)}
