b5660590fc392b1d4652ae6ca080f254989d23d0
clever-king-402 <122508802+clever-king-402@users.noreply.github.com>
2023-03-08 21:50:55 +0545

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App text-4xl text-purple-200 font-bold ">
+      hello world my Code Works
+    </div>
+  );
+}
+
+export default App;
